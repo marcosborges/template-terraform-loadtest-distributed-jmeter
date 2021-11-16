6ab8671f1802e795c03143844211d152dfa76ea7
Marcos Borges <contato@marcosborges.com>
2021-11-16 01:11:36 +0000

    terraform fmt

--- iac/main.tf
+++ iac/main.tf
@@ -1,14 +1,14 @@
 module "loadtest" {
 
-    source  = "marcosborges/loadtest-distribuited/aws"
+  source = "marcosborges/loadtest-distribuited/aws"
 
-    name = "execution-name-locust"
+  name = "execution-name-locust"
 
-    executor = var.executor
-    loadtest_dir_source = "../plans/"
-    nodes_size = 2
-    
-    loadtest_entrypoint = <<-EOT
+  executor            = var.executor
+  loadtest_dir_source = "../plans/"
+  nodes_size          = 2
+
+  loadtest_entrypoint = <<-EOT
         jmeter \
             -n \
             -t basic.jmx  \
@@ -18,5 +18,5 @@ module "loadtest" {
             -Dserver.rmi.ssl.disable=true
     EOT
 
-    subnet_id = data.aws_subnet.current.id
+  subnet_id = data.aws_subnet.current.id
 }
