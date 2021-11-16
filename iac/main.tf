a6481ab29938afdd1046c2fe15d9b7b3016c9818
Marcos Borges <contato@marcosborges.com>
2021-11-16 01:03:03 +0000

    first commit

---
+++ iac/main.tf
@@ -0,0 +1,22 @@
+module "loadtest" {
+
+    source  = "marcosborges/loadtest-distribuited/aws"
+
+    name = "execution-name-locust"
+
+    executor = var.executor
+    loadtest_dir_source = "../plans/"
+    nodes_size = 2
+    
+    loadtest_entrypoint = <<-EOT
+        jmeter \
+            -n \
+            -t basic.jmx  \
+            -R \"{NODES_IPS}\" \
+            -l /loadtest/logs -e -o /var/www/html/jmeter \
+            -Dnashorn.args=--no-deprecation-warning \
+            -Dserver.rmi.ssl.disable=true
+    EOT
+
+    subnet_id = data.aws_subnet.current.id
+}
