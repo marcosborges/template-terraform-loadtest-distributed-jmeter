5a830a4d34742c20b40c7ab914694a27ca332849
Marcos Borges <contato@marcosborges.com>
2021-11-18 04:16:47 +0000

    ...

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-example-update-here"
+    #bucket = "tfstate-example-update-here" # Defined by pipeline 
     key    = "path/to/my/terraform.tfstate"
     region = "us-east-1"
   }
